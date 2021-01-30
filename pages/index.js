71d9ed872807a7e22944c3e192391524f54ce2f6
Nick Cammarata <cammarata.nick@gmail.com>
2021-01-30 18:24:35 -0500

    pre-render banner

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,10 @@ import { MDXProvider } from '@mdx-js/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Article from './article'
-import Banner from './banner'
 
 window.process = 'development'
 window.React = React
 
-ReactDOM.render(<Banner />, document.querySelector('d-title'))
 ReactDOM.render(
   <Article
     components={{
